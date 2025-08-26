bbf029aa58861ff4e5952d7a591d72bd057322e0
Anurag Giri <anuraggiri4308@gmail.com>
2025-08-27 00:44:09 +0530

    vite.config.ts changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/", // ← CHANGE THIS
   css: {
     postcss: {
       plugins: [tailwind()],
